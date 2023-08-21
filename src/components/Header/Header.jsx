952a70b36c68b92c0584c6e350eae882809e5b2d
Stephanie Hall <stephanie.anie.hall@gmail.com>
2023-08-20 21:20:57 -0700

    Set up header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./Header.scss";
+
+function Header() {
+    return (
+        <section className="site-header">
+            <section className="site-header__logo">
+
+            </section>
+            <section className="site-header__nav">
+
+            </section>
+        </section>
+    )
+}
+
+export default Header;
