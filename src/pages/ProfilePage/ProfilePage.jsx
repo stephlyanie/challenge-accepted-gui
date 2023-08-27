4bc106091c9d1935a309091de9e2913dd617937e
Stephanie Hall <stephanie.anie.hall@gmail.com>
2023-08-27 11:56:41 -0700

    Added gallery to profile page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -3,27 +3,57 @@ import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
 import ProfileCard from "../../components/ProfileCard/ProfileCard";
+import SimpleGallery from "../../components/SimpleGallery/SimpleGallery";
 
 function ProfilePage() {
 
-    const [data, setData] = useState([]);
     const { userId } = useParams();
+    const [data, setData] = useState([]);
+    const [gallery, setGallery] = useState([]);
+    const [galleryTitle, setGalleryTitle] = useState(``);
 
     useEffect(() => {
         axios
             .get(`http://localhost:8080/users/${userId}`)
             .then((res) => {
                 setData(res.data[0]);
+                setGalleryTitle(`Creations by ${res.data[0].username}`)
             })
             .catch((error) => {
                 console.log(error);
             })
     }, [])
 
+    useEffect(() => {
+        axios
+        .get(`http://localhost:8080/users/${userId}/creations`)
+        .then((res) => {
+            setGallery(res.data)
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }, []);
+
+    // useEffect(() => {
+    //     axios
+    //     .get(`http://localhost:8080/users/${userId}/challenges`)
+    //     .then((res) => {
+    //         setChallenges(res.data);
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     })
+    // }, []);
 
     return (
         <div>
             <ProfileCard data={data} />
+            <div className="">
+                <button id="creations">Creations</button>
+                <button id="challenges">Challenges</button>
+                {gallery.length > 0 ? <SimpleGallery galleryTitle={galleryTitle} gallery={gallery} /> : <p>None here yet.</p>}
+            </div>
         </div>
     )
 };
