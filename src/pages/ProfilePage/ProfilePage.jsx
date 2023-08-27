5711370429b2a0394ba6812796c496ce6c80fe74
Stephanie Hall <stephanie.anie.hall@gmail.com>
2023-08-27 12:31:20 -0700

    Set up toggle buttons for gallery tabs and added functionality comments to code

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -7,52 +7,107 @@ import SimpleGallery from "../../components/SimpleGallery/SimpleGallery";
 
 function ProfilePage() {
 
-    const { userId } = useParams();
-    const [data, setData] = useState([]);
-    const [gallery, setGallery] = useState([]);
-    const [galleryTitle, setGalleryTitle] = useState(``);
+    const { userId } = useParams(); // Pulls userId from URL
+ 
+//
+// Profile Component
+//
 
+    const [data, setData] = useState([]); // Stores profile data to pass to profile component
+    
     useEffect(() => {
         axios
             .get(`http://localhost:8080/users/${userId}`)
             .then((res) => {
                 setData(res.data[0]);
-                setGalleryTitle(`Creations by ${res.data[0].username}`)
             })
             .catch((error) => {
                 console.log(error);
             })
     }, [])
 
+//
+// Simple Gallery Component
+//
+
+    const [gallery, setGallery] = useState([]); // Stores creations or challenges data from single user and passes to simple gallery component
+    const [galleryTitle, setGalleryTitle] = useState(``); // Stores gallery title to pass to simple gallery component
+    const [creationsTab, setCreationsTab] = useState(true); // Stores the gallery creations tab as true/false
+    const [challengesTab, setChallengesTab] = useState(false); // Stores the gallery challenges tab as true/false
+
+    // Calls database to set the simple gallery to challenges or creations data from user ID
+    // Listens for creationsTab and challengesTab changes
+    
     useEffect(() => {
+
+        // If the challenges tab is true (challenges button clicked)
+        // Sets the simple gallery data as list of challenges from the user
+        // Set gallery title to creations
+
+        if (challengesTab) {
+            axios
+            .get(`http://localhost:8080/users/${userId}/challenges`)
+            .then((res) => {
+                setGallery(res.data)
+                setGalleryTitle(`Challenges by ${data.username}`)
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        }
+
+        // On load and if the creations tab is true (creations button clicked)
+        // Sets the simple gallery data as list of creations from the user
+        // Set gallery title to creations
+
         axios
         .get(`http://localhost:8080/users/${userId}/creations`)
         .then((res) => {
             setGallery(res.data)
+            setGalleryTitle(`Creations by ${data.username}`)
         })
         .catch((error) => {
             console.log(error);
         })
-    }, []);
+    }, [creationsTab, challengesTab]);
 
-    // useEffect(() => {
-    //     axios
-    //     .get(`http://localhost:8080/users/${userId}/challenges`)
-    //     .then((res) => {
-    //         setChallenges(res.data);
-    //     })
-    //     .catch((error) => {
-    //         console.log(error);
-    //     })
-    // }, []);
+    // Button handler for creations tab
+    // Sets creationsTab to true on click
+    // Toggles ChallengesTab to false
+
+    const handleCreationsTab = () => {
+        setCreationsTab(true);
+        setChallengesTab(false);
+    }
+
+
+    // Button handler for challenges tab
+    // Sets challengesTab to true on click
+    // Toggles CreationsTab to false
+
+    const handleChallengesTab = () => {
+        setCreationsTab(false);
+        setChallengesTab(true);
+    }
+
+//
+// Renders to Page
+//
 
     return (
         <div>
+            {/* Profile Component */}
             <ProfileCard data={data} />
+            
             <div className="">
-                <button id="creations">Creations</button>
-                <button id="challenges">Challenges</button>
+
+                {/* Gallery Tab Buttons */}
+                <button id="creations" onClick={handleCreationsTab}>Creations</button>
+                <button id="challenges" onClick={handleChallengesTab}>Challenges</button>
+                
+                {/* Simple Gallery Component */}
                 {gallery.length > 0 ? <SimpleGallery galleryTitle={galleryTitle} gallery={gallery} /> : <p>None here yet.</p>}
+            
             </div>
         </div>
     )
