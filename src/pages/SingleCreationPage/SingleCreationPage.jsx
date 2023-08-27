2b5e674f987407e273ab689987a6a7e3599b61b0
Stephanie Hall <stephanie.anie.hall@gmail.com>
2023-08-27 12:53:52 -0700

    Added a list of creations by the same user

--- src/pages/SingleCreationPage/SingleCreationPage.jsx
+++ src/pages/SingleCreationPage/SingleCreationPage.jsx
@@ -3,27 +3,49 @@ import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
 import DetailsCard from "../../components/DetailsCard/DetailsCard";
+import SimpleGallery from "../../components/SimpleGallery/SimpleGallery";
 
 function SingleCreationPage() {
 
     const [data, setData] = useState([]);
     const { creationId } = useParams();
+    const [userId, setUserId] = useState();
+    const [gallery, setGallery] = useState([]);
+    const [galleryTitle, setGalleryTitle] = useState(``); // Stores gallery title to pass to simple gallery component
+
 
     useEffect(() => {
         axios
             .get(`http://localhost:8080/creations/${creationId}`)
             .then((res) => {
                 setData(res.data[0]);
+                console.log(res.data[0].created_by_id)
+                setUserId(res.data[0].created_by_id)
+                console.log(userId)
             })
             .catch((error) => {
                 console.log(error);
             })
     }, [])
 
+    useEffect(() => {
+        axios
+            .get(`http://localhost:8080/users/${userId}/creations`)
+            .then((res) => {
+                setGallery(res.data);
+                setGalleryTitle(`More creations by ${data.username}`)
+                // To do filter out current creation
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, [userId])
+
 
     return (
         <div>
             <DetailsCard data={data} />
+            {gallery.length > 0 ? <SimpleGallery galleryTitle={galleryTitle} gallery={gallery} /> : <p></p>}
         </div>
     )
 };
