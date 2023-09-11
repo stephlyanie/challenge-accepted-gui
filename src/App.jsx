b4681252bd38e5f6ecb5efd8ec9fd403ffd640c0
Stephanie Hall <stephanie.anie.hall@gmail.com>
2023-09-10 18:27:30 -0700

    Created search bar placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Search from "./components/Search/Search";
 
 import ChallengesPage from "./pages/Challenges/ChallengesPage";
 import SingleChallengePage from "./pages/Challenges/SingleChallengePage";
@@ -16,56 +17,85 @@ import ProfilePage from "./pages/Profile/ProfilePage";
 
 import CreatePage from "./pages/CreatePage/CreatePage";
 
-
-
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <div className="body-container">
+      <div className="container">
         <aside>
-
+          <Search />
         </aside>
         <main>
           <Routes>
-          {/* ROOT ROUTE */}
+            {/* ROOT ROUTE */}
             <Route path="/" element={<ChallengesPage />}></Route>
-
-          {/* CHALLENGES ROUTES */}
-            <Route path="/challenges" element={<ChallengesPage />}></Route> {/* list of all challenges */}
-            <Route path="/challenges/:challengeId" element={<SingleChallengePage />}></Route> {/* single challenge */}
-            <Route path="/challenges/:challengeId/edit" element={<EditChallengePage />}></Route> {/* edit single challenge if logged in */}
-            <Route path="/challenges/:challengeId/delete"></Route> {/* delete single challenge if logged in */}
-            <Route path="/challenges/:challengeId/create" element={<SpecificChallengeCreationPage />} ></Route> {/* create a specific challenge */}
-        
-          {/* CREATIONS ROUTES */}
-            <Route path="/creations" element={<CreationsPage />}></Route> {/* list of all creations */}
-            <Route path="/creations/:creationId" element={<SingleCreationPage />}></Route> {/* single creation */}
-            <Route path="/creations/:creationId/edit" element={<EditCreationPage />}></Route> {/* edit single creation if logged in */}
-            <Route path="/creations/:creationId/delete"></Route> {/* delete single creation if logged in */}
-            
-          {/* PROFILE ROUTES */}
-            <Route path="/profile" element={<Navigate to='/profile/DADDA3C9-C1F8-4BAF-AC74-05AABED44DD5' />}></Route> {/* redirect to profile/:id */}
-            <Route path="/profile/:userId" element={<ProfilePage />}></Route> {/* user's profile when logged in */}
-            <Route path="/profile/:userId/edit"></Route> {/* edit profile if logged in */}
-            <Route path="/profile/:userId/delete"></Route> {/* delete profile if logged in */}
-            <Route path="/profile/:userId/challenges"></Route> {/* list of all challenges associated with user's profile*/}
-            <Route path="/profile/:userId/creations"></Route> {/* list of all creations associated with user's profile*/}
-            
-          {/* CREATE ROUTE */}
+            {/* CHALLENGES ROUTES */}
+            <Route path="/challenges" element={<ChallengesPage />}></Route>{" "}
+            {/* list of all challenges */}
+            <Route
+              path="/challenges/:challengeId"
+              element={<SingleChallengePage />}
+            ></Route>{" "}
+            {/* single challenge */}
+            <Route
+              path="/challenges/:challengeId/edit"
+              element={<EditChallengePage />}
+            ></Route>{" "}
+            {/* edit single challenge if logged in */}
+            <Route
+              path="/challenges/:challengeId/create"
+              element={<SpecificChallengeCreationPage />}
+            ></Route>{" "}
+            {/* create a specific challenge */}
+            {/* CREATIONS ROUTES */}
+            <Route path="/creations" element={<CreationsPage />}></Route>{" "}
+            {/* list of all creations */}
+            <Route
+              path="/creations/:creationId"
+              element={<SingleCreationPage />}
+            ></Route>{" "}
+            {/* single creation */}
+            <Route
+              path="/creations/:creationId/edit"
+              element={<EditCreationPage />}
+            ></Route>{" "}
+            {/* edit single creation if logged in */}
+            {/* PROFILE ROUTES */}
+            <Route
+              path="/profile"
+              element={
+                <Navigate to="/profile/DADDA3C9-C1F8-4BAF-AC74-05AABED44DD5" />
+              }
+            ></Route>{" "}
+            {/* redirect to profile/:id */}
+            <Route
+              path="/profile/:userId"
+              element={<ProfilePage />}
+            ></Route>{" "}
+            {/* user's profile when logged in */}
+            <Route path="/profile/:userId/edit"></Route>{" "}
+            {/* edit profile if logged in */}
+            <Route path="/profile/:userId/challenges"></Route>{" "}
+            {/* list of all challenges associated with user's profile*/}
+            <Route path="/profile/:userId/creations"></Route>{" "}
+            {/* list of all creations associated with user's profile*/}
+            {/* CREATE ROUTE */}
             <Route path="/create" element={<CreatePage />}></Route>
-          <Route path="/create/challenge" element={<CreateChallengePage />}></Route>
-            <Route path="/create/creation" element={<CreateCreationPage />}></Route>
-          
-          {/* SIGN UP ROUTE */}
+            <Route
+              path="/create/challenge"
+              element={<CreateChallengePage />}
+            ></Route>
+            <Route
+              path="/create/creation"
+              element={<CreateCreationPage />}
+            ></Route>
+            {/* SIGN UP ROUTE */}
             <Route path="/signup"></Route>
-
-          {/* LOG IN ROUTE */}
+            {/* LOG IN ROUTE */}
             <Route path="/login"></Route>
           </Routes>
-
         </main>
       </div>
     </BrowserRouter>
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,12 @@
+import "./Search.scss";
+
+function Search() {
+  // Renders to page
+  return (
+    <section className="search">
+      <input type="search" className="search__input" id="search"></input>
+    </section>
+  );
+}
+
+export default Search;
